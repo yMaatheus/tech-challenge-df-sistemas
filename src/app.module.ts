298b6c21193fbde8b9d6e7961ec2daaeea936d5c
Matheus <profissional.ymatheus@gmail.com>
2025-06-14 20:42:14 -0300

    refactor: remove unnecessary files and modules
    - Deleted package.json to remove project dependencies and scripts.
    - Added nest-cli.json for NestJS configuration.
    - Removed app.module.ts, main.ts, and products module files to clean up the project structure.
    - Deleted jest-e2e.json, tsconfig.build.json, and tsconfig.json for a fresh start.

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductsModule } from '@modules/products/products.module';
-
-@Module({
-  imports: [ProductsModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
