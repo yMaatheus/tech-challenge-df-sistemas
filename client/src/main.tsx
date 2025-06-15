4c8c87d93e1870d227fd32eed8fb09d4df2c628c
Matheus <profissional.ymatheus@gmail.com>
2025-06-15 14:37:58 -0300

    feat: update dependencies and refactor app structure
    - Added new Radix UI components for alert dialogs, avatars, dialogs, and dropdown menus.
    - Introduced date-fns for date manipulation and ky for HTTP requests.
    - Integrated next-themes for theme management and sonner for notifications.
    - Removed the old App component and replaced it with a Router component wrapped in BrowserRouter for improved routing.
    - Updated tsconfig to disable erasableSyntaxOnly for better TypeScript compatibility.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { Router } from "@/pages/router.tsx";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </StrictMode>,
 );
