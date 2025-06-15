210ef4a587a7f52b857e7962f6ef608875174ca4
Matheus <profissional.ymatheus@gmail.com>
2025-06-15 03:46:41 -0300

    feat: create ProductModule to manage product use cases

---
+++ server/src/app/product/product.module.ts
@@ -0,0 +1,34 @@
+import { CreateProductUseCase } from '@app/product/use-cases/create-product.usecase'
+import { GetProductUseCase } from '@app/product/use-cases/get-product.usecase'
+import { ListProductsUseCase } from '@app/product/use-cases/list-products.usecase'
+import { RemoveProductUseCase } from '@app/product/use-cases/remove-product.usecase'
+import { UpdateProductUseCase } from '@app/product/use-cases/update-product.usecase'
+import { ProductRepository } from '@interface/mongoose/product/product.repository'
+import {
+  Product,
+  ProductSchema,
+} from '@interface/mongoose/schemas/product.schema'
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
+  providers: [
+    ProductRepository,
+    CreateProductUseCase,
+    UpdateProductUseCase,
+    ListProductsUseCase,
+    GetProductUseCase,
+    RemoveProductUseCase,
+  ],
+  exports: [
+    CreateProductUseCase,
+    UpdateProductUseCase,
+    ListProductsUseCase,
+    GetProductUseCase,
+    RemoveProductUseCase,
+  ],
+})
+export class ProductModule {}
