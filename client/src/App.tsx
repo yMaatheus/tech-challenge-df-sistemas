877bf9387d84c3fbad57a10f9f8a03d87c1ef1fd
Matheus <profissional.ymatheus@gmail.com>
2025-06-14 22:30:41 -0300

    feat(client): initialize React app with Vite and React setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   app.use(
-    '/reference',
+    '/docs',
     apiReference({
       spec: {
         content: document,
