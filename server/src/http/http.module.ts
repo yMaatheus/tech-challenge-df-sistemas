6c6bebf59e17e66a4ab5e78ab81c9251a79499de
Matheus <profissional.ymatheus@gmail.com>
2025-06-15 03:52:23 -0300

    feat: integrate ReviewModule and related controllers into HttpModule

--- server/src/http/http.module.ts
+++ server/src/http/http.module.ts
@@ -1,4 +1,5 @@
 import { ProductModule } from '@app/product/product.module'
+import { ReviewModule } from '@app/review/review.module'
 import {
   CreateProductController,
 } from '@http/controllers/product/create-product.controller'
@@ -12,16 +13,28 @@ import {
 import {
   UpdateProductController,
 } from '@http/controllers/product/update-product.controller'
+import {
+  AverageRatingController,
+} from '@http/controllers/review/average-rating.controller'
+import { CreateReviewController } from '@http/controllers/review/create-review.controller'
+import { ListReviewsController } from '@http/controllers/review/list-reviews.controller'
+import { RemoveReviewController } from '@http/controllers/review/remove-review.controller'
+import { UpdateReviewController } from '@http/controllers/review/update-review.controller'
 import { Module } from '@nestjs/common'
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, ReviewModule],
   controllers: [
     CreateProductController,
     ListProductsController,
     GetProductController,
     UpdateProductController,
     RemoveProductController,
+    AverageRatingController,
+    CreateReviewController,
+    ListReviewsController,
+    RemoveReviewController,
+    UpdateReviewController,
   ],
 })
 export class HttpModule {}
