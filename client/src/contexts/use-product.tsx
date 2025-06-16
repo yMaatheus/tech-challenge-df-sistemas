ad5fd9b77fa445e74ce2f0fa81cc56adc78a23ab
Matheus <ymatheus.h@gmail.com>
2025-06-16 06:40:01 -0300

    feat: add product creation and update functions to ProductContext

--- client/src/contexts/use-product.tsx
+++ client/src/contexts/use-product.tsx
@@ -1,17 +1,31 @@
+import type { ProductSubmit } from "@/components/app/product-form";
 import { createContext, useContext, type PropsWithChildren } from "react";
 
 export interface ContextType {
+  handleCreateProduct: (product: ProductSubmit) => Promise<void>;
+  handleUpdateProduct: (product: ProductSubmit) => Promise<void>;
   handleDeleteProduct: (productId: string) => Promise<void>;
 }
 
 export const ProductContext = createContext({} as ContextType);
 
 export const ProductProvider = ({ children }: PropsWithChildren) => {
+
+  async function handleCreateProduct(product: ProductSubmit) {
+    console.log('create product: ' + product);
+  }
+
+  async function handleUpdateProduct(product: ProductSubmit) {
+    console.log('update product: ' + product);
+  }
+
   async function handleDeleteProduct(productId: string) {
-    console.log('hello world ' + productId);
+    console.log('delete product: ' + productId);
   }
 
   const value = {
+    handleCreateProduct,
+    handleUpdateProduct,
     handleDeleteProduct,
   };
 
