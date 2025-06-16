f50d354e3c4dbeeea71d4f5fc8e49980d33ae3f2
Matheus <ymatheus.h@gmail.com>
2025-06-16 09:45:45 -0300

    feat: enhance DetailsProductPage with review creation and display functionality

--- client/src/pages/products/details/details-product.tsx
+++ client/src/pages/products/details/details-product.tsx
@@ -1,17 +1,10 @@
-import { FetchStatus, type IProduct } from "@/common";
+import { FetchStatus, type IProduct, type IReview } from "@/common";
+import { DeleteAlertDialog } from "@/components/app/delete-alert-dialog";
 import { Header } from "@/components/app/header";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Badge } from "@/components/ui/badge";
+import { ProductAboutDetails } from "@/components/app/product-about-details";
+import { ProductForm } from "@/components/app/product-form";
+import { ReviewForm, type ReviewSubmit } from "@/components/app/review-form";
+import { ReviewsList } from "@/components/app/reviews-list/reviews-list";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -22,17 +15,26 @@ import {
 } from "@/components/ui/card";
 import { useProduct } from "@/contexts/use-product";
 import { useFetch } from "@/hooks/use-fetch";
+import { createReview } from "@/services/create-review";
 import { fetchProductById } from "@/services/fetch-product-by-id";
-import { getReviewsAverage, type getReviewsAverageResponse } from "@/services/get-reviews-average";
-import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
-import { ArrowLeft, Pencil, Plus, Star, Trash2 } from "lucide-react";
-import { Link, Navigate, useParams } from "react-router";
+import { fetchReviewsByProductId } from "@/services/fetch-reviews-by-product";
+import {
+  getReviewsAverage,
+  type getReviewsAverageResponse,
+} from "@/services/get-reviews-average";
+import { ArrowLeft, Pencil, Plus, Trash2 } from "lucide-react";
+import { Link, Navigate, useNavigate, useParams } from "react-router";
+import { toast } from "sonner";
 
 export function DetailsProductPage() {
+  const navigate = useNavigate();
   const { productId } = useParams();
-  const { handleDeleteProduct } = useProduct();
-  const { status, data: product } = useFetch<IProduct, string>({
+  const { handleUpdateProduct, handleDeleteProduct } = useProduct();
+  const {
+    status,
+    data: product,
+    refetch,
+  } = useFetch<IProduct, string>({
     callback: fetchProductById,
     params: productId!,
   });
@@ -40,11 +42,36 @@ export function DetailsProductPage() {
     callback: getReviewsAverage,
     params: productId!,
   });
+  const fetchReviews = useFetch<IReview[], string>({
+    callback: fetchReviewsByProductId,
+    params: productId!,
+  });
 
   const average = reviewsAverage?.average;
 
   if (!product && status !== FetchStatus.LOADING) {
     return <Navigate to="/products" />;
+  }
+
+  async function handleCreateReview(data: ReviewSubmit) {
+    try {
+      const promise = createReview({
+        productId: data.productId,
+        author: data.author,
+        rating: data.rating,
+        comment: data.comment,
+      });
+
+      toast.promise(promise, {
+        loading: "Carregando...",
+        success: "Avaliação publicada com sucesso.",
+      });
+
+      return promise;
+    } catch (error) {
+      toast.error("Não foi possível publicar uma avaliação. Tente novamente.");
+    }
+    return null;
   }
 
   return (
@@ -58,133 +85,72 @@ export function DetailsProductPage() {
             Voltar
           </Link>
         </Button>
+
         <div className="flex gap-2">
-          <Button
-            variant="outline"
-            // onClick={() => setModalProdutoAberto(true)}
-          >
-            <Pencil className="mr-2 h-4 w-4" />
-            Editar Produto
-          </Button>
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
+          <ProductForm
+            product={product}
+            submit={async (product) => {
+              const result = await handleUpdateProduct(product);
+              await refetch();
+
+              return result;
+            }}
+            triggerBtn={
+              <Button variant="outline">
+                <Pencil className="mr-2 h-4 w-4" />
+                Editar Produto
+              </Button>
+            }
+          />
+
+          <DeleteAlertDialog
+            title="Excluir produto"
+            description={`Tem certeza que deseja excluir o produto "${product?.name}"? Esta ação não pode ser desfeita e todas as avaliações associadas também serão excluídas.`}
+            onClick={async () => {
+              await handleDeleteProduct(productId!);
+              navigate("/products");
+            }}
+            triggerBtn={
               <Button variant="destructive">
                 <Trash2 className="mr-2 h-4 w-4" />
                 Excluir Produto
               </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Excluir produto</AlertDialogTitle>
-                <AlertDialogDescription>
-                  Tem certeza que deseja excluir o produto "{product?.name}"?
-                  Esta ação não pode ser desfeita e todas as avaliações
-                  associadas também serão excluídas.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                <AlertDialogAction
-                  onClick={() => handleDeleteProduct(product?._id!)}
-                  className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
-                >
-                  Excluir
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
+            }
+          />
         </div>
       </div>
 
       <div className="grid gap-6">
-        <Card>
-          <CardHeader>
-            <div className="flex justify-between items-start">
-              <div>
-                <CardTitle className="text-2xl">{product?.name}</CardTitle>
-                <CardDescription>
-                  Categoria:{" "}
-                  <Badge variant="outline">{product?.category}</Badge>
-                </CardDescription>
-              </div>
-              <div className="text-right">
-                <div className="text-2xl font-bold">
-                  R$ {product?.price.toFixed(2)}
-                </div>
-
-                {average && (
-                  <div className="flex items-center mt-1 text-sm text-muted-foreground">
-                    <div className="flex">
-                      {[1, 2, 3, 4, 5].map((star) => (
-                        <Star
-                          key={star}
-                          className={`h-4 w-4 ${
-                            star <= Math.floor(average)
-                              ? "fill-primary text-primary"
-                              : "fill-muted text-muted-foreground"
-                          }`}
-                        />
-                      ))}
-                    </div>
-                    <span className="ml-1">({average.toFixed(1)})</span>
-                  </div>
-                )}
-              </div>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <div className="space-y-4">
-              <div>
-                <h3 className="font-medium mb-2">Descrição</h3>
-                <p className="text-muted-foreground">{product?.description}</p>
-              </div>
-
-              <div className="grid grid-cols-2 gap-4 pt-4">
-                <div>
-                  <h3 className="font-medium mb-1">ID do Produto</h3>
-                  <p className="text-sm text-muted-foreground">
-                    {product?._id}
-                  </p>
-                </div>
-                <div>
-                  <h3 className="font-medium mb-1">Data de Criação</h3>
-                  <p className="text-sm text-muted-foreground">
-                    {product?.createdAt &&
-                      format(
-                        new Date(product?.createdAt),
-                        "dd 'de' MMMM 'de' yyyy",
-                        { locale: ptBR },
-                      )}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
+        <ProductAboutDetails product={product} reviewsAverage={average} />
 
         <Card>
           <CardHeader className="flex flex-row items-center justify-between">
-            <div>
+            <div className="flex flex-col gap-2">
               <CardTitle>Avaliações</CardTitle>
               <CardDescription>
                 Avaliações dos clientes sobre este produto
               </CardDescription>
             </div>
-            
-            <Button
-            // onClick={() => setModalAvaliacaoAberto(true)}
-            >
-              <Plus className="mr-2 h-4 w-4" />
-              Nova Avaliação
-            </Button>
-          </CardHeader>
-          <CardContent>
-            {/* <ListaAvaliacoes
-              produtoId={id}
-              onAvaliacaoModificada={() => {
-                carregarMediaAvaliacoes()
+
+            <ReviewForm
+              productId={productId!}
+              submit={async (data) => {
+                const result = await handleCreateReview(data);
+                await fetchReviews.refetch();
+
+                return result;
               }}
-            /> */}
+              triggerBtn={
+                <Button>
+                  <Plus className="mr-2 h-4 w-4" />
+                  Nova Avaliação
+                </Button>
+              }
+            />
+          </CardHeader>
+
+          <CardContent>
+            <ReviewsList fetchReviews={fetchReviews} />
           </CardContent>
         </Card>
       </div>
