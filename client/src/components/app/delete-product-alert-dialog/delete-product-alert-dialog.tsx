dd12759c3f95b061f4b3241e16c2169fafe422c6
Matheus <ymatheus.h@gmail.com>
2025-06-16 06:13:15 -0300

    feat: add DeleteProductAlertDialog component for product deletion confirmation

---
+++ client/src/components/app/delete-product-alert-dialog/delete-product-alert-dialog.tsx
@@ -0,0 +1,48 @@
+import type { IProduct } from "@/common";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useProduct } from "@/contexts/use-product";
+import { Trash2 } from "lucide-react";
+
+export function DeleteProductAlertDialog({ product }: { product: IProduct }) {
+  const { handleDeleteProduct } = useProduct()
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Trash2 className="h-4 w-4" />
+          <span className="sr-only">Excluir</span>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Excluir produto</AlertDialogTitle>
+          <AlertDialogDescription>
+            Tem certeza que deseja excluir o produto "{product.name}
+            "? Esta ação não pode ser desfeita.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => handleDeleteProduct(product._id)}
+            className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+          >
+            Excluir
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
