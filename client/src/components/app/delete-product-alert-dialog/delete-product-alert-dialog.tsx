5b44f06dbed93e6646865ebe4c7801668ebf7026
Matheus <ymatheus.h@gmail.com>
2025-06-16 06:38:12 -0300

    feat: enhance DeleteProductAlertDialog to accept custom trigger button and improve product deletion confirmation message

--- client/src/components/app/delete-product-alert-dialog/delete-product-alert-dialog.tsx
+++ client/src/components/app/delete-product-alert-dialog/delete-product-alert-dialog.tsx
@@ -10,33 +10,34 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
 import { useProduct } from "@/contexts/use-product";
-import { Trash2 } from "lucide-react";
+import type { ReactNode } from "react";
 
-export function DeleteProductAlertDialog({ product }: { product: IProduct }) {
-  const { handleDeleteProduct } = useProduct()
+export function DeleteProductAlertDialog({
+  product,
+  triggerBtn,
+}: {
+  product?: IProduct;
+  triggerBtn: ReactNode;
+}) {
+  const { handleDeleteProduct } = useProduct();
 
   return (
     <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Trash2 className="h-4 w-4" />
-          <span className="sr-only">Excluir</span>
-        </Button>
-      </AlertDialogTrigger>
+      <AlertDialogTrigger asChild>{triggerBtn}</AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Excluir produto</AlertDialogTitle>
           <AlertDialogDescription>
-            Tem certeza que deseja excluir o produto "{product.name}
-            "? Esta ação não pode ser desfeita.
+            Tem certeza que deseja excluir o produto "{product?.name}"? Esta
+            ação não pode ser desfeita e todas as avaliações associadas também
+            serão excluídas.
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancelar</AlertDialogCancel>
           <AlertDialogAction
-            onClick={() => handleDeleteProduct(product._id)}
+            onClick={() => handleDeleteProduct(product?._id!)}
             className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
           >
             Excluir
